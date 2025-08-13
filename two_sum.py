859eebc17e2ec7f043c68eb8476ef3362c299b87
Sameer Ahmed <sameerhawaldar10@gmail.com>
2025-08-13 23:17:50 +0530

    Add two_sum.py solution in Arrays

---
+++ two_sum.py
@@ -0,0 +1,8 @@
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        if target - num in hashmap:
+            return [hashmap[target - num], i]
+        hashmap[num] = i
+
+print(two_sum([2,7,11,15], 9))
